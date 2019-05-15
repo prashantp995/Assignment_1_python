23cbf7e0f1356a5fefcee395c351d9ab87a10a33
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-05-15 14:01:15 -0400

    Added guess optoion

--- guess.py
+++ guess.py
@@ -23,12 +23,22 @@ class guess:
                 wordToGuess = stringDatabase.database.getWord(stringDatabase.database)
                 currentGuess = list("----")
             if choice == "l":
+                print("please enter the letter")
                 letter = input()[0]
                 if letter in wordToGuess:
                     print("your one character is correct")
                     # ref :https://stackoverflow.com/questions/3873361/finding-multiple-occurrences-of-a-string-within-a-string-in-python
                     for index in re.finditer(letter, str(wordToGuess)):
                         currentGuess[index.start()] = letter
+            if choice == "g":
+                print("please enter your guessed string")
+                guessedstring = input()
+                if guessedstring == wordToGuess:
+                    print("your guess is correct")
+                    wordToGuess = stringDatabase.database.getWord(stringDatabase.database)
+                    currentGuess = list("----")
+                else:
+                    print("your guess is not correct")
 
 
 guess.game(guess)
