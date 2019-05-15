b296889d3865da571ef72710b895078dc00635c6
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-05-15 11:18:39 -0400

    getting input from user

--- guess.py
+++ guess.py
@@ -1,13 +1,31 @@
 import stringDatabase
 import random
+import re
 
 
 class guess:
     def game(self):
-        gamequite = False;
+        gamequite = False
+        wordToGuess = list(stringDatabase.database.getWord(stringDatabase.database))
+        currentGuess = list("----")
+        print(wordToGuess)
         while gamequite != True:
-            wordToGuess = stringDatabase.database.getWord(stringDatabase.database);
-            print(wordToGuess);
+
+            print("Current guess is")
+            print(''.join(currentGuess))
+            print("g= guess, t = tellme, l for a letter, and q to quit")
+            choice = str(input())
+            if choice == "q":
+                print("Quiting the game")
+                gamequite = True
+            if choice == "l":
+                letter = input()[0]
+                if letter in wordToGuess:
+                    print("your one character is correct")
+                    indexToChange = wordToGuess.index(letter)
+                    currentGuess[indexToChange] = letter
+                    for m in re.finditer(letter, wordToGuess):
+                        print('ll found', m.start(), m.end())
 
 
 guess.game(guess)
