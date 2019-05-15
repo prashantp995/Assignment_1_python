65b6c8abc43e0734dff179189fbd56c02119c59f
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-05-15 16:04:34 -0400

    scoring mechanism WIP

---
+++ game.py
@@ -0,0 +1,56 @@
+wordFrequency = {'a': 8.17,
+                 'b': 1.49,
+                 'c': 2.78,
+                 'd': 4.25,
+                 'e': 12.70,
+                 'f': 2.23,
+                 'g': 2.02,
+                 'h': 6.09,
+                 'i': 6.97,
+                 'j': 0.15,
+                 'k': 0.77,
+                 'l': 4.03,
+                 'm': 2.41,
+                 'n': 6.75,
+                 'o': 7.51,
+                 'p': 1.93,
+                 'q': 0.10,
+                 'r': 5.99,
+                 's': 6.33,
+                 't': 9.06,
+                 'u': 2.76,
+                 'v': 0.98,
+                 'w': 2.36,
+                 'x': 0.15,
+                 'y': 1.97,
+                 'z': 0.07
+                 }
+
+
+class game:
+    gameList = []
+
+    def __init__(self, word=None, status=None, badguess=None, missedletters=None, score=None):
+        self.word = word
+        self.status = status
+        self.badguess = badguess
+        self.missedletters = missedletters
+        self.score = score
+
+    def getScore(self, input):
+        print("getting score for ", input)
+        return wordFrequency.get(input.lower())
+
+    def printGameList(self):
+        print("Game     word        status      Bad guess      missedletters                score")
+        # ref :https://stackoverflow.com/questions/3249524/print-in-one-line-dynamically
+        counter = 0
+        for game in self.gameList:
+            print(counter, end="")
+            print("\t\t", game.word, end=" ")
+            print("\t\t\t", game.status, end=" ")
+            print("\t\t\t\t", game.badguess, end=" ")
+            print("\t\t\t\t\t", game.missedletters, end=" ")
+            print("\t\t\t\t\t", game.score, end=" ")
+            counter = counter + 1
+            print("\n")
--- guess.py
+++ guess.py
@@ -1,3 +1,4 @@
+import game
 import stringDatabase
 import random
 import re
@@ -8,28 +9,47 @@ class guess:
         gamequit = False
         wordToGuess = stringDatabase.database.getWord(stringDatabase.database)
         currentGuess = list("----")
+        score = 0
+        badguess = 0
+        missedletter = 0
+        status = ""
         print(wordToGuess)
         while gamequit != True:
-
+            if ('-' not in currentGuess):
+                game.game.gameList.append(game.game(wordToGuess, status, badguess, missedletter, score))
+                wordToGuess = stringDatabase.database.getWord(stringDatabase.database)
+                currentGuess = list("----")
+                badguess = 0
+                missedletter = 0
             print("Current guess is")
             print(''.join(currentGuess))
+            print("wordToGuess is ", wordToGuess)
             print("g = guess, t = tellme, l for a letter, and q to quit")
             choice = str(input())
             if choice == "q":
                 print("Quiting the game")
+                game.game.printGameList(game.game);
                 gamequit = True
             if choice == "t":
                 print("word is " + wordToGuess)
                 wordToGuess = stringDatabase.database.getWord(stringDatabase.database)
                 currentGuess = list("----")
+                badguess = 0
+                missedletter = 0
             if choice == "l":
                 print("please enter the letter")
                 letter = input()[0]
+                characterscore = game.game.getScore(game.game, letter)
+                print("character score is ", characterscore)
                 if letter in wordToGuess:
                     print("your one character is correct")
                     # ref :https://stackoverflow.com/questions/3873361/finding-multiple-occurrences-of-a-string-within-a-string-in-python
                     for index in re.finditer(letter, str(wordToGuess)):
+                        score = score + characterscore
                         currentGuess[index.start()] = letter
+                else:
+                    score = score - characterscore;
+                    missedletter = missedletter + 1
             if choice == "g":
                 print("please enter your guessed string")
                 guessedstring = input()
@@ -38,6 +58,7 @@ class guess:
                     wordToGuess = stringDatabase.database.getWord(stringDatabase.database)
                     currentGuess = list("----")
                 else:
+                    badguess = badguess + 1
                     print("your guess is not correct")
 
 
